6b25eaee957df33d21fc498c8899bbca5432290b
Christoph Schmatzler <christoph@schmatzler.com>
2026-03-26 16:28:19 +0000

    fix(mail): request Gmail labels scope

--- packages/core/src/mail/gmail/oauth.ts
+++ packages/core/src/mail/gmail/oauth.ts
@@ -9,6 +9,7 @@ import { Config, Effect, Layer, Redacted, ServiceMap } from "effect";
 
 const GMAIL_SCOPES = [
 	"https://www.googleapis.com/auth/gmail.readonly",
+	"https://www.googleapis.com/auth/gmail.labels",
 	"https://www.googleapis.com/auth/userinfo.email",
 ].join(" ");
 
