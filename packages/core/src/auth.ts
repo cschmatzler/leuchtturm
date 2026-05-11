699a11f0b11ba554fd13b06409f5c66b6d34ac11
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-11 19:51:38 +0000

    Inline auth factory

--- packages/core/src/auth.ts
+++ packages/core/src/auth.ts
@@ -88,303 +88,298 @@ export namespace Auth {
 			const email = yield* Email.Service;
 			const context = new AsyncLocalStorage<Context.Context<never>>();
 
-			const makeAuth = (runAuthEffect: <A, E>(effect: Effect.Effect<A, E>) => Promise<A>) =>
-				betterAuth({
-					baseURL: `https://${Resource.Dns.ApiDomain}/auth`,
-					trustedOrigins: [`https://${Resource.Dns.AppDomain}`],
-					secret: Resource.BetterAuthSecret.value,
-					onAPIError: {
-						throw: true,
+			const runAuthEffect = <A, E>(effect: Effect.Effect<A, E>) =>
+				Effect.runPromiseWith(context.getStore() ?? Context.empty())(effect);
+
+			const auth = betterAuth({
+				baseURL: `https://${Resource.Dns.ApiDomain}/auth`,
+				trustedOrigins: [`https://${Resource.Dns.AppDomain}`],
+				secret: Resource.BetterAuthSecret.value,
+				onAPIError: {
+					throw: true,
+				},
+				database: drizzleAdapter(rawDatabase, {
+					provider: "pg",
+					schema: {
+						account: accountTable,
+						session: sessionTable,
+						user: userTable,
+						verification: verificationTable,
+						organization: organizationTable,
+						member: memberTable,
+						invitation: invitationTable,
+						team: teamTable,
+						teamMember: teamMemberTable,
 					},
-					database: drizzleAdapter(rawDatabase, {
-						provider: "pg",
-						schema: {
-							account: accountTable,
-							session: sessionTable,
-							user: userTable,
-							verification: verificationTable,
-							organization: organizationTable,
-							member: memberTable,
-							invitation: invitationTable,
-							team: teamTable,
-							teamMember: teamMemberTable,
+				}),
+				user: {
+					additionalFields: {
+						language: {
+							type: "string",
+							required: false,
+							default: "en",
 						},
+					},
+				},
+				socialProviders: {
+					google: {
+						clientId: Resource.GoogleClientId.value,
+						clientSecret: Resource.GoogleClientSecret.value,
+					},
+				},
+				plugins: [
+					adminPlugin(),
+					multiSession(),
+					magicLink({
+						storeToken: "hashed",
+						sendMagicLink: ({ email: emailAddress, url }) =>
+							runAuthEffect(
+								sendMagicLinkEmail({
+									email: emailAddress,
+									signInUrl: url,
+									send: (params) => email.send(params),
+								}).pipe(
+									Effect.catchCause((cause) =>
+										Effect.gen(function* () {
+											yield* Effect.annotateCurrentSpan({
+												"error.original_cause": Cause.pretty(cause),
+											});
+											return yield* Effect.fail(new AuthMagicLinkEmailError());
+										}),
+									),
+								),
+							),
 					}),
-					user: {
-						additionalFields: {
-							language: {
-								type: "string",
-								required: false,
-								default: "en",
-							},
-						},
-					},
-					socialProviders: {
-						google: {
-							clientId: Resource.GoogleClientId.value,
-							clientSecret: Resource.GoogleClientSecret.value,
-						},
-					},
-					plugins: [
-						adminPlugin(),
-						multiSession(),
-						magicLink({
-							storeToken: "hashed",
-							sendMagicLink: ({ email: emailAddress, url }) =>
-								runAuthEffect(
-									sendMagicLinkEmail({
-										email: emailAddress,
-										signInUrl: url,
-										send: (params) => email.send(params),
-									}).pipe(
-										Effect.catchCause((cause) =>
-											Effect.gen(function* () {
-												yield* Effect.annotateCurrentSpan({
-													"error.original_cause": Cause.pretty(cause),
-												});
-												return yield* Effect.fail(new AuthMagicLinkEmailError());
-											}),
-										),
+					organizationPlugin({
+						sendInvitationEmail: (params) =>
+							runAuthEffect(
+								sendInvitationEmail({
+									acceptUrl: `https://${Resource.Dns.AppDomain}/accept-invitation/${params.id}`,
+									email: params.email,
+									inviterName: params.inviter.user.name,
+									organizationName: params.organization.name,
+									send: (params) => email.send(params),
+								}).pipe(
+									Effect.catchCause((cause) =>
+										Effect.gen(function* () {
+											yield* Effect.annotateCurrentSpan({
+												"error.original_cause": Cause.pretty(cause),
+											});
+											return yield* Effect.fail(new AuthInvitationEmailError());
+										}),
 									),
 								),
-						}),
-						organizationPlugin({
-							sendInvitationEmail: (params) =>
-								runAuthEffect(
-									sendInvitationEmail({
-										acceptUrl: `https://${Resource.Dns.AppDomain}/accept-invitation/${params.id}`,
-										email: params.email,
-										inviterName: params.inviter.user.name,
-										organizationName: params.organization.name,
-										send: (params) => email.send(params),
-									}).pipe(
-										Effect.catchCause((cause) =>
-											Effect.gen(function* () {
-												yield* Effect.annotateCurrentSpan({
-													"error.original_cause": Cause.pretty(cause),
-												});
-												return yield* Effect.fail(new AuthInvitationEmailError());
-											}),
-										),
-									),
-								),
-							teams: {
+							),
+						teams: {
+							enabled: true,
+							defaultTeam: {
 								enabled: true,
-								defaultTeam: {
-									enabled: true,
-								},
 							},
-							schema: {
-								team: {
-									additionalFields: {
-										slug: {
-											type: "string",
-											required: true,
-										},
+						},
+						schema: {
+							team: {
+								additionalFields: {
+									slug: {
+										type: "string",
+										required: true,
 									},
 								},
 							},
-							organizationHooks: {
-								beforeCreateOrganization: ({ organization }) =>
-									runAuthEffect(
-										Effect.gen(function* () {
-											const organizationName = yield* Schema.decodeEffect(
-												OrganizationInsert.fields.name,
-											)(organization.name ?? "");
-											const existingOrganization = yield* database
-												.select({ id: organizationTable.id })
-												.from(organizationTable)
-												.where(eq(organizationTable.slug, organizationName.toLowerCase()))
-												.limit(1);
+						},
+						organizationHooks: {
+							beforeCreateOrganization: ({ organization }) =>
+								runAuthEffect(
+									Effect.gen(function* () {
+										const organizationName = yield* Schema.decodeEffect(
+											OrganizationInsert.fields.name,
+										)(organization.name ?? "");
+										const existingOrganization = yield* database
+											.select({ id: organizationTable.id })
+											.from(organizationTable)
+											.where(eq(organizationTable.slug, organizationName.toLowerCase()))
+											.limit(1);
 
-											if (existingOrganization.length > 0) {
-												return yield* new AuthDuplicateOrganizationNameError();
-											}
+										if (existingOrganization.length > 0) {
+											return yield* new AuthDuplicateOrganizationNameError();
+										}
 
-											return {
-												data: {
-													...organization,
-													name: organizationName,
-													slug: organizationName.toLowerCase(),
-												},
-											};
+										return {
+											data: {
+												...organization,
+												name: organizationName,
+												slug: organizationName.toLowerCase(),
+											},
+										};
+									}),
+								),
+							afterCreateOrganization: ({ organization, user }) =>
+								runAuthEffect(
+									billing.createCustomer({
+										organizationId: organization.id,
+										name: organization.name,
+										slug: organization.slug,
+										ownerEmail: user.email,
+										ownerName: user.name,
+									}),
+								),
+							beforeUpdateOrganization: ({ organization, member }) =>
+								runAuthEffect(
+									Effect.logInfo("Auth organization update requested").pipe(
+										Effect.annotateLogs({
+											organizationId: member.organizationId,
+											updatedFields: Object.keys(organization).sort().join(","),
 										}),
 									),
-								afterCreateOrganization: ({ organization, user }) =>
-									runAuthEffect(
-										billing.createCustomer({
-											organizationId: organization.id,
-											name: organization.name,
-											slug: organization.slug,
-											ownerEmail: user.email,
-											ownerName: user.name,
-										}),
-									),
-								beforeUpdateOrganization: ({ organization, member }) =>
-									runAuthEffect(
-										Effect.logInfo("Auth organization update requested").pipe(
-											Effect.annotateLogs({
-												organizationId: member.organizationId,
-												updatedFields: Object.keys(organization).sort().join(","),
-											}),
-										),
-									),
-								afterUpdateOrganization: ({ organization }) => {
-									const updatedOrganization = organization!;
+								),
+							afterUpdateOrganization: ({ organization }) => {
+								const updatedOrganization = organization!;
 
-									return runAuthEffect(
-										Effect.gen(function* () {
-											yield* Effect.logInfo("Auth organization updated").pipe(
-												Effect.annotateLogs({ organizationId: updatedOrganization.id }),
-											);
+								return runAuthEffect(
+									Effect.gen(function* () {
+										yield* Effect.logInfo("Auth organization updated").pipe(
+											Effect.annotateLogs({ organizationId: updatedOrganization.id }),
+										);
 
-											return yield* billing.updateCustomer({
-												organizationId: updatedOrganization.id,
-												name: updatedOrganization.name,
-												slug: updatedOrganization.slug,
-											});
-										}),
-									);
-								},
-								beforeCreateTeam: ({ team, organization }) =>
-									runAuthEffect(
-										Effect.gen(function* () {
-											const teamName = yield* Schema.decodeEffect(TeamInsert.fields.name)(
-												team.name,
-											);
-											const existingTeam = yield* database
-												.select({ id: teamTable.id })
-												.from(teamTable)
-												.where(
-													and(
-														eq(teamTable.organizationId, organization.id),
-														eq(teamTable.slug, teamName.toLowerCase()),
-													),
-												)
-												.limit(1);
+										return yield* billing.updateCustomer({
+											organizationId: updatedOrganization.id,
+											name: updatedOrganization.name,
+											slug: updatedOrganization.slug,
+										});
+									}),
+								);
+							},
+							beforeCreateTeam: ({ team, organization }) =>
+								runAuthEffect(
+									Effect.gen(function* () {
+										const teamName = yield* Schema.decodeEffect(TeamInsert.fields.name)(team.name);
+										const existingTeam = yield* database
+											.select({ id: teamTable.id })
+											.from(teamTable)
+											.where(
+												and(
+													eq(teamTable.organizationId, organization.id),
+													eq(teamTable.slug, teamName.toLowerCase()),
+												),
+											)
+											.limit(1);
 
-											if (existingTeam.length > 0) {
-												return yield* new AuthDuplicateTeamNameError();
-											}
+										if (existingTeam.length > 0) {
+											return yield* new AuthDuplicateTeamNameError();
+										}
 
-											return {
-												data: {
-													...team,
-													name: teamName,
-													slug: teamName.toLowerCase(),
-													organizationId: organization.id,
-												},
-											};
-										}),
-									),
-								beforeUpdateTeam: ({ team, updates, organization }) =>
-									runAuthEffect(
-										Effect.gen(function* () {
-											yield* Effect.logInfo("Auth team update requested").pipe(
-												Effect.annotateLogs({
-													teamId: team.id,
-													organizationId: organization.id,
-													updatedFields: Object.keys(updates).sort().join(","),
-												}),
-											);
-
-											if (updates.name === undefined) {
-												return { data: updates };
-											}
-
-											const teamName = yield* Schema.decodeEffect(TeamInsert.fields.name)(
-												updates.name,
-											);
-											const existingTeam = yield* database
-												.select({ id: teamTable.id })
-												.from(teamTable)
-												.where(
-													and(
-														ne(teamTable.id, team.id),
-														eq(teamTable.slug, teamName.toLowerCase()),
-														eq(teamTable.organizationId, organization.id),
-													),
-												)
-												.limit(1);
-
-											if (existingTeam.length > 0) {
-												return yield* new AuthDuplicateTeamNameError();
-											}
-
-											return {
-												data: {
-													...updates,
-													name: teamName,
-													slug: teamName.toLowerCase(),
-												},
-											};
-										}),
-									),
-								afterUpdateTeam: ({ team, organization }) =>
-									runAuthEffect(
-										Effect.logInfo("Auth team updated").pipe(
-											Effect.annotateLogs({
-												teamId: team!.id,
+										return {
+											data: {
+												...team,
+												name: teamName,
+												slug: teamName.toLowerCase(),
 												organizationId: organization.id,
+											},
+										};
+									}),
+								),
+							beforeUpdateTeam: ({ team, updates, organization }) =>
+								runAuthEffect(
+									Effect.gen(function* () {
+										yield* Effect.logInfo("Auth team update requested").pipe(
+											Effect.annotateLogs({
+												teamId: team.id,
+												organizationId: organization.id,
+												updatedFields: Object.keys(updates).sort().join(","),
 											}),
-										),
-									),
-							},
-						}),
-						billing.authPlugin,
-					],
-					session: {
-						cookieCache: {
-							enabled: true,
-							maxAge: 5 * 60,
-						},
-					},
-					advanced: {
-						disableOriginCheck: true,
-						crossSubDomainCookies: {
-							enabled: true,
-							domain: Resource.Dns.AppDomain,
-						},
-						database: {
-							generateId: ({ model }) => {
-								switch (model) {
-									case "account":
-										return Schema.decodeSync(AccountSelect.fields.id)(`acc_${ulid()}`);
-									case "user":
-										return Schema.decodeSync(UserSelect.fields.id)(`usr_${ulid()}`);
-									case "session":
-										return Schema.decodeSync(SessionSelect.fields.id)(`ses_${ulid()}`);
-									case "verification":
-										return Schema.decodeSync(VerificationSelect.fields.id)(`ver_${ulid()}`);
-									case "organization":
-										return Schema.decodeSync(OrganizationSelect.fields.id)(`org_${ulid()}`);
-									case "member":
-										return Schema.decodeSync(MemberSelect.fields.id)(`mem_${ulid()}`);
-									case "invitation":
-										return Schema.decodeSync(InvitationSelect.fields.id)(`inv_${ulid()}`);
-									case "team":
-										return Schema.decodeSync(TeamSelect.fields.id)(`tea_${ulid()}`);
-									case "teamMember":
-										return Schema.decodeSync(TeamMemberSelect.fields.id)(`tmb_${ulid()}`);
-									default:
-										throw new Error(`Unknown auth model: ${model}`);
-								}
-							},
-						},
-					},
-				});
+										);
 
-			const auth = makeAuth((effect) =>
-				Effect.runPromiseWith(context.getStore() ?? Context.empty())(effect),
-			);
+										if (updates.name === undefined) {
+											return { data: updates };
+										}
+
+										const teamName = yield* Schema.decodeEffect(TeamInsert.fields.name)(
+											updates.name,
+										);
+										const existingTeam = yield* database
+											.select({ id: teamTable.id })
+											.from(teamTable)
+											.where(
+												and(
+													ne(teamTable.id, team.id),
+													eq(teamTable.slug, teamName.toLowerCase()),
+													eq(teamTable.organizationId, organization.id),
+												),
+											)
+											.limit(1);
+
+										if (existingTeam.length > 0) {
+											return yield* new AuthDuplicateTeamNameError();
+										}
+
+										return {
+											data: {
+												...updates,
+												name: teamName,
+												slug: teamName.toLowerCase(),
+											},
+										};
+									}),
+								),
+							afterUpdateTeam: ({ team, organization }) =>
+								runAuthEffect(
+									Effect.logInfo("Auth team updated").pipe(
+										Effect.annotateLogs({
+											teamId: team!.id,
+											organizationId: organization.id,
+										}),
+									),
+								),
+						},
+					}),
+					billing.authPlugin,
+				],
+				session: {
+					cookieCache: {
+						enabled: true,
+						maxAge: 5 * 60,
+					},
+				},
+				advanced: {
+					disableOriginCheck: true,
+					crossSubDomainCookies: {
+						enabled: true,
+						domain: Resource.Dns.AppDomain,
+					},
+					database: {
+						generateId: ({ model }) => {
+							switch (model) {
+								case "account":
+									return Schema.decodeSync(AccountSelect.fields.id)(`acc_${ulid()}`);
+								case "user":
+									return Schema.decodeSync(UserSelect.fields.id)(`usr_${ulid()}`);
+								case "session":
+									return Schema.decodeSync(SessionSelect.fields.id)(`ses_${ulid()}`);
+								case "verification":
+									return Schema.decodeSync(VerificationSelect.fields.id)(`ver_${ulid()}`);
+								case "organization":
+									return Schema.decodeSync(OrganizationSelect.fields.id)(`org_${ulid()}`);
+								case "member":
+									return Schema.decodeSync(MemberSelect.fields.id)(`mem_${ulid()}`);
+								case "invitation":
+									return Schema.decodeSync(InvitationSelect.fields.id)(`inv_${ulid()}`);
+								case "team":
+									return Schema.decodeSync(TeamSelect.fields.id)(`tea_${ulid()}`);
+								case "teamMember":
+									return Schema.decodeSync(TeamMemberSelect.fields.id)(`tmb_${ulid()}`);
+								default:
+									throw new Error(`Unknown auth model: ${model}`);
+							}
+						},
+					},
+				},
+			});
 
 			const handle = Effect.fn("Auth.handle")(function* (request: Request) {
 				const currentContext = yield* Effect.context<never>();
 
-				return yield* Effect.tryPromise({
-					try: () => context.run(currentContext, () => auth.handler(request)),
-					catch: (cause) => cause,
-				});
+				return yield* Effect.promise(() =>
+					context.run(currentContext, () => auth.handler(request)),
+				);
 			});
 
 			const getSession = Effect.fn("Auth.getSession")(function* (headers: Headers) {
