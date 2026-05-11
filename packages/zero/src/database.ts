b131e5cb9af5ade05a05181d671ee5815441964a
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-11 21:50:13 +0000

    Rename Zero database service

--- packages/zero/src/database.ts
+++ packages/zero/src/database.ts
@@ -3,21 +3,19 @@ import * as Context from "effect/Context";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 
-import { Database } from "@leuchtturm/core/drizzle";
+import { Database as CoreDatabase } from "@leuchtturm/core/drizzle";
 import { schema } from "@leuchtturm/zero/schema";
 
-export namespace ZeroPushDatabase {
+export namespace Database {
 	export interface Interface {
-		readonly database: ReturnType<typeof zeroDrizzle<typeof schema, Database.RawDatabase>>;
+		readonly database: ReturnType<typeof zeroDrizzle<typeof schema, CoreDatabase.RawDatabase>>;
 	}
 
-	export class Service extends Context.Service<Service, Interface>()(
-		"@leuchtturm/zero/ZeroPushDatabase",
-	) {}
+	export class Service extends Context.Service<Service, Interface>()("@leuchtturm/zero/Database") {}
 
 	export const layer = Layer.effect(Service)(
 		Effect.gen(function* () {
-			const { rawDatabase } = yield* Database.Service;
+			const { rawDatabase } = yield* CoreDatabase.Service;
 
 			return Service.of({
 				database: zeroDrizzle(schema, rawDatabase),
