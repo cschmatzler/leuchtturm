fdc6f7c558af8a09afe62892af936c7e85b61e0c
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-13 15:23:14 +0000

    Preserve hurl test file output

--- scripts/test-api.fish
+++ scripts/test-api.fish
@@ -12,25 +12,38 @@ set base_url https://api.$stage.leuchtturm.dev
 set auth_email hurl@leuchtturm.dev
 set auth_password api-test-password
 set organization_id org_01KRGY2EF40Y4QAG4MGQ9B9YMP
-set test_file (mktemp --suffix=.hurl)
 set cookie_jar (mktemp)
+set exit_code 0
 
 function cleanup --on-event fish_exit
-	rm -f $test_file $cookie_jar
+	rm -f $cookie_jar
 end
 
-for file in apps/api/tests/*.hurl apps/api/tests/setup/*.hurl apps/api/tests/authenticated/*.hurl
-	cat $file >> $test_file
-	echo >> $test_file
+hurl --test \
+	--variable BASE_URL=$base_url \
+	apps/api/tests/*.hurl
+if test $status -ne 0
+	set exit_code 1
+end
+
+hurl --test \
+	--cookie-jar $cookie_jar \
+	--variable BASE_URL=$base_url \
+	--variable AUTH_EMAIL=$auth_email \
+	--variable AUTH_PASSWORD=$auth_password \
+	apps/api/tests/setup/*.hurl
+if test $status -ne 0
+	exit 1
 end
 
 hurl --test \
 	--cookie $cookie_jar \
 	--cookie-jar $cookie_jar \
 	--variable BASE_URL=$base_url \
-	--variable AUTH_EMAIL=$auth_email \
-	--variable AUTH_PASSWORD=$auth_password \
 	--variable ORGANIZATION_ID=$organization_id \
-	$test_file
+	apps/api/tests/authenticated/*.hurl
+if test $status -ne 0
+	set exit_code 1
+end
 
-exit $status
+exit $exit_code
