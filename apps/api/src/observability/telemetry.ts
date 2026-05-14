541afaa82b31bf3694a25d418a02592245224eeb
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-14 14:16:45 +0000

    Use Effect OTLP telemetry exporter

--- apps/api/src/observability/telemetry.ts
+++ apps/api/src/observability/telemetry.ts
@@ -9,9 +9,7 @@ import * as EffectTracer from "effect/Tracer";
 import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
 import * as HttpMiddleware from "effect/unstable/http/HttpMiddleware";
 import * as HttpServerRequest from "effect/unstable/http/HttpServerRequest";
-import * as OtlpLogger from "effect/unstable/observability/OtlpLogger";
-import * as OtlpMetrics from "effect/unstable/observability/OtlpMetrics";
-import * as OtlpSerialization from "effect/unstable/observability/OtlpSerialization";
+import * as Otlp from "effect/unstable/observability/Otlp";
 import { Resource } from "sst/resource/cloudflare";
 
 import { RequestContext } from "@leuchtturm/api/middleware/request-context";
@@ -48,40 +46,22 @@ export namespace Telemetry {
 				}),
 			),
 		),
-		Layer.mergeAll(
-			OtlpLogger.layer({
-				excludeLogSpans: false,
-				headers: {
-					Authorization: JSON.parse(Resource.GrafanaOtlpConfig.value).authorization,
+		Otlp.layerProtobuf({
+			baseUrl: JSON.parse(Resource.GrafanaOtlpConfig.value).url,
+			headers: {
+				Authorization: JSON.parse(Resource.GrafanaOtlpConfig.value).authorization,
+			},
+			loggerMergeWithExisting: true,
+			resource: {
+				serviceName: "leuchtturm-api",
+				attributes: {
+					"service.namespace": "leuchtturm",
+					app: "leuchtturm",
+					stage: Resource.App.stage,
 				},
-				mergeWithExisting: true,
-				resource: {
-					serviceName: "leuchtturm-api",
-					attributes: {
-						"service.namespace": "leuchtturm",
-						app: "leuchtturm",
-						stage: Resource.App.stage,
-					},
-				},
-				shutdownTimeout: "3 seconds",
-				url: `${JSON.parse(Resource.GrafanaOtlpConfig.value).url}/v1/logs`,
-			}),
-			OtlpMetrics.layer({
-				headers: {
-					Authorization: JSON.parse(Resource.GrafanaOtlpConfig.value).authorization,
-				},
-				resource: {
-					serviceName: "leuchtturm-api",
-					attributes: {
-						"service.namespace": "leuchtturm",
-						app: "leuchtturm",
-						stage: Resource.App.stage,
-					},
-				},
-				shutdownTimeout: "3 seconds",
-				url: `${JSON.parse(Resource.GrafanaOtlpConfig.value).url}/v1/metrics`,
-			}),
-		).pipe(Layer.provide(OtlpSerialization.layerProtobuf), Layer.provide(FetchHttpClient.layer)),
+			},
+			shutdownTimeout: "3 seconds",
+		}).pipe(Layer.provide(FetchHttpClient.layer)),
 		Layer.succeed(
 			HttpMiddleware.SpanNameGenerator,
 			(request: HttpServerRequest.HttpServerRequest) =>
