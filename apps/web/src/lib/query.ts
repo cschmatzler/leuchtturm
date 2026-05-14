f1f4a1b8ec9d1ccd6731cf8f9b80dc2f96bf0f25
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-14 11:09:11 +0000

    Fix Zero React import path

--- apps/web/src/lib/query.ts
+++ apps/web/src/lib/query.ts
@@ -1,5 +1,4 @@
+import { useQuery as useZeroQuery } from "@rocicorp/zero/react";
 import { useQuery as useReactQuery } from "@tanstack/react-query";
-
-import { useQuery as useZeroQuery } from "@leuchtturm/zero/react";
 
 export { useReactQuery, useZeroQuery };
