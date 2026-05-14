7bf81803e819d19541babf35fe4e9209bd6c4f26
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-14 14:06:44 +0000

    Restore API logs and metrics export

--- apps/api/src/observability/telemetry.ts
+++ apps/api/src/observability/telemetry.ts
@@ -6,8 +6,12 @@ import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 import * as Option from "effect/Option";
 import * as EffectTracer from "effect/Tracer";
+import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
 import * as HttpMiddleware from "effect/unstable/http/HttpMiddleware";
 import * as HttpServerRequest from "effect/unstable/http/HttpServerRequest";
+import * as OtlpLogger from "effect/unstable/observability/OtlpLogger";
+import * as OtlpMetrics from "effect/unstable/observability/OtlpMetrics";
+import * as OtlpSerialization from "effect/unstable/observability/OtlpSerialization";
 import { Resource } from "sst/resource/cloudflare";
 
 import { RequestContext } from "@leuchtturm/api/middleware/request-context";
@@ -44,6 +48,40 @@ export namespace Telemetry {
 				}),
 			),
 		),
+		Layer.mergeAll(
+			OtlpLogger.layer({
+				excludeLogSpans: false,
+				headers: {
+					Authorization: JSON.parse(Resource.GrafanaOtlpConfig.value).authorization,
+				},
+				mergeWithExisting: true,
+				resource: {
+					serviceName: "leuchtturm-api",
+					attributes: {
+						"service.namespace": "leuchtturm",
+						app: "leuchtturm",
+						stage: Resource.App.stage,
+					},
+				},
+				shutdownTimeout: "3 seconds",
+				url: `${JSON.parse(Resource.GrafanaOtlpConfig.value).url}/v1/logs`,
+			}),
+			OtlpMetrics.layer({
+				headers: {
+					Authorization: JSON.parse(Resource.GrafanaOtlpConfig.value).authorization,
+				},
+				resource: {
+					serviceName: "leuchtturm-api",
+					attributes: {
+						"service.namespace": "leuchtturm",
+						app: "leuchtturm",
+						stage: Resource.App.stage,
+					},
+				},
+				shutdownTimeout: "3 seconds",
+				url: `${JSON.parse(Resource.GrafanaOtlpConfig.value).url}/v1/metrics`,
+			}),
+		).pipe(Layer.provide(OtlpSerialization.layerProtobuf), Layer.provide(FetchHttpClient.layer)),
 		Layer.succeed(
 			HttpMiddleware.SpanNameGenerator,
 			(request: HttpServerRequest.HttpServerRequest) =>
